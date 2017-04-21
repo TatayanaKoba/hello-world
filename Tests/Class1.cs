6962276f31436eb92088616b19647b4526642dee
TatayanaKoba <tatayana_v@mail.ru>
2017-04-21 21:32:31 +0300

    Commit message

---
+++ Tests/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Class1
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void OneTest()
+        {
+            Console.WriteLine("Hi!");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
